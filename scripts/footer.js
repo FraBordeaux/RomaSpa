70daedc5c33b3df1413f095459fe03446e7d8262
Francesca <francesca.nadel@gmail.com>
2022-05-23 22:40:17 +0200

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,1 +1,30 @@
+// retrieve footer section
+const footer = document.querySelector("footer");
 
+footer.innerHTML =
+`
+<div class="top-section">
+<div class="left-contact-info">
+    <p> <strong> Roma Spa </strong> </p>
+    <p>Bahnhofplatz 18</p>
+    <p>3920 Zermatt – Switzerland</p>
+    <a href="tel:+41279664400">+41 27 966 44 00</a>
+    <a href="mailto:info@schlosshotelzermatt.ch">info@schlosshotelzermatt.ch</a>
+</div>
+
+<div class="right-links">
+    <a href="contact.html">Contact</a>
+    <a href="press.html">Presse</a>
+    <a href="careers.html">Carrières</a>
+    <a href="team.html">Equipe</a>
+</div>
+</div>
+
+<div class="social-networks">Ajouter réseaux sociaux</div>
+<div class="bottom-of-page">Ajouter copyright, politics, ...</div>
+
+
+`;
+
+
+// modify html and inject
