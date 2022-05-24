1e4e2548df059e960738be3cb5f5d47322eb15c7
FraBordeaux <francesca.nadel@gmail.com>
2022-05-24 11:21:28 +0200

    modified treatments

--- scripts/treatments.js
+++ scripts/treatments.js
@@ -1,1 +1,6 @@
+// retrieve main section
+const pricingList = document.querySelector(".pricing-list");
+const vipPricingList = document.querySelector(".vip-pricing-list");
 
+pricingList.innerHTML = ``;
+vipPricingList.innerHTML = ``;
