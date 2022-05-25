fea6eb2f5e38597b48899542c7bb66e3000b14a7
FraBordeaux <francesca.nadel@gmail.com>
2022-05-25 14:57:46 +0200

    cart works, lots of bugs

--- scripts/products.js
+++ scripts/products.js
@@ -4,41 +4,64 @@ let allProducts = [];
 
 class Product {
     productCategory;
+    productImage;
     productName;
     productDescription;
     productPrice;
     productVolume;
 
-    constructor(productCategory, productName, productDescription, productPrice, productVolume){
+    constructor(productCategory, image, productName, productDescription, productPrice, productVolume){
         this.productCategory = productCategory;
+        this.productImage = image;
         this.productName = productName;
         this.productDescription = productDescription;
         this.productPrice = productPrice;
         this.productVolume = productVolume;
         allProducts.push(this);
     }
+
+    placeInCart(){
+
+    }
 }
 
-let kitVoyage = new Product("Kit", "Collection Rituels de Beauté", ` Partez à la découverte des recettes de beauté ancestrales avec cette collection de 5 incontournables visage et corps. Des formats voyage rassemblés dans une charmante trousse qui ne quittera plus votre sac !`, 39, 15-30);
+let kitVoyage = new Product("Kit", "/assets/images/products/kit-voyage.jpg", "Collection Rituels de Beauté", ` Partez à la découverte des recettes de beauté ancestrales avec cette collection de 5 incontournables visage et corps. Des formats voyage rassemblés dans une charmante trousse qui ne quittera plus votre sac !`, 39, "15-30");
+let kitVoyage2 = new Product("Kit", "/assets/images/products/kit-voyage.jpg", "Collection Rituels de Beauté", ` Partez à la découverte des recettes de beauté ancestrales avec cette collection de 5 incontournables visage et corps. Des formats voyage rassemblés dans une charmante trousse qui ne quittera plus votre sac !`, 39, "15-30");
 
 
 const productList = document.querySelector(".product-list");
 
-
+// push product(object) to html
 for(let i = 0 ; i < allProducts.length ; i ++){
 
         productList.innerHTML +=
         `
             <div>
-                    <img src="/assets/images/products/kit-voyage.jpg" /> 
+                    <img src="${allProducts[i].productImage}" /> 
                     <h4>${allProducts[i].productName}</h4>
                     <p>${allProducts[i].productDescription}</p>
-                    <p>${allProducts[i].productPrice} min</p>
-                    <p>${allProducts[i].productVolume} €</p>
-                    <button>Acheter</button>
+                    <p>${allProducts[i].productPrice}€</p>
+                    <p>${allProducts[i].productVolume}ml</p>
+                    <button class="button-buy" id="button${[i]}" name="${allProducts[i].productName}" value="${allProducts[i].productPrice}">Acheter</button>
                 
             </div>
         
         `;
 }
+
+// get user information
+let retrievedUser = localStorage.getItem('user'); // local json string
+let userLoggedIn = JSON.parse(retrievedUser); // changed to object
+
+// retrieve all buy buttons
+const allBuyButtons = document.querySelectorAll(".button-buy");
+
+// if buy button clicked, add product to user cart.
+allBuyButtons.forEach(item =>{
+    item.addEventListener("click", addToCart =>{
+        userLoggedIn.userCart.push(item.name, item.value);
+        console.log(("item added to cart :" + userLoggedIn.userCart));
+        localStorage.setItem('user', JSON.stringify(userLoggedIn));
+    })
+})
 
