e15ca285abdb2105b44022518dc2b0453df9a8d9
FraBordeaux <103116149+FraBordeaux@users.noreply.github.com>
2022-05-25 22:45:06 +0200

    improved login pages, account pages

--- scripts/account.js
+++ scripts/account.js
@@ -1,82 +1,36 @@
-const userEmail = document.querySelector('#user-email'); // login page, user email input
-const userPassword = document.querySelector('#user-password'); // login page, user password input
-const userLogIn = document.querySelector('#login'); // login page, login button
 const userLogOut = document.querySelector('#logout'); // account page, logout button
-const userSignUp = document.querySelector('#sign-up'); // loginpage, sign up button
-
 const allTabButtons = document.querySelectorAll('.tabs button'); // account page, all tab buttons
 const payButton = document.querySelector('#pay'); // account page, pay button
 const reserveTreatment = document.querySelector('#reserve-treatment'); // account page, reserve button
 const reserveInactive = document.querySelector('#reserve-inactive'); // inactive reserve form
 const reserveActive = document.querySelector('#reserve-active'); // active reserve form
-
 const selectedProducts = document.querySelector(".selected-products"); // account block, product section
 
-const loggedOut = document.querySelector(".logged-out"); // login block active
-const loggedIn = document.querySelector(".logged-in"); // account block active
-
-// class user
-class User {
-    userFirstName;
-    userLastName;
-    userEmail;
-    userTel;
-    userMobile;
-    userDateOfBirth;
-    userAddresses=[];
-    userCart=[];
-    userCartTotal=0;
-    userTreatment=[];
-    userFollowOrder=[];
-    userHistory=[];
-    loggedIn = false;
-
-    constructor(firstName, lastName, addresses, email, tel, mobile, dob){
-        this.userFirstName = firstName;
-        this.userLastName = lastName;
-        this.userAddresses = addresses;
-        this.userEmail = email;
-        this.userTel = tel;
-        this.userMobile = mobile;
-        this.userDateOfBirth = dob;
-    }
-
-    updateCart(){
-        let retrieveUserInfo = localStorage.getItem('user'); // format string
-        let userObj = JSON.parse(retrieveUserInfo);// format objet
-        
-        if((userObj.userCart.length) === 0){
-            buttonPlaceHolder();
-        } else {
-            selectedProductList(userObj.userCart);
-        }
-    }// end updateCart function
-         
-} // end class
-
-// create user
-let userFra = new User("Francesca", "Nadel", {"domicile":"Bordeaux"}, "fra@gmail.com", "0622000452", "0622000452", "30/05/1977")
+let retrieveUserInfo = localStorage.getItem('user'); // format string
+let user = JSON.parse(retrieveUserInfo);// format objet
 
 // user logged in 
-userLogIn.addEventListener("click", logIn =>{
-    userFra.loggedIn = true;    // user is logged in
-    loggedOut.style.display = "none"; // remove login block
-    loggedIn.style.display = "block"; // display account block
-    document.querySelector("h2").innerText = `Bienvenue ${userFra.userFirstName}!`; // welcome message
-    localStorage.setItem('user', JSON.stringify(userFra)); // create user in localStorage
-})
+document.querySelector("h2").innerText = `Bienvenue ${user.userFirstName}!`; // welcome message
+
+function updateCart(){
+    if((user.userCart.length) === 0){
+        buttonPlaceHolder();
+    } else {
+        selectedProductList(user.userCart);
+    }
+}// end updateCart function
+
+// update user cart when page refreshed
+updateCart();
 
 // user logged out
 userLogOut.addEventListener("click", logOut =>{
-    userFra.loggedIn = false;
-    loggedOut.style.display = "block";
-    loggedIn.style.display = "none";
-    
+    user.loggedIn = false;
+    localStorage.clear;
+    window.location.href = "/pages/log-in.html";
 })
 
-// update user cart when page refreshed
-userFra.updateCart();
-
+// if cart empty, show link to product page
 function buttonPlaceHolder(){
     selectedProducts.innerHTML =
     `
@@ -85,7 +39,8 @@ function buttonPlaceHolder(){
     </div>
     `;
 }
-        
+
+// if full, show list of products in cart
 function selectedProductList(cart){
     selectedProducts.innerHTML =
     `
@@ -93,7 +48,6 @@ function selectedProductList(cart){
         <h3>Voici les produits dans votre panier:</h3>
     `;
 
-    
     cart.forEach(item =>{
         selectedProducts.innerHTML +=
         `
@@ -104,7 +58,6 @@ function selectedProductList(cart){
         `;
     })
         
-    
     selectedProducts.innerHTML +=
         `
         <div class="cart-total">
--- scripts/log-in.js
+++ scripts/log-in.js
@@ -1,5 +1,56 @@
-const userEmail = document.querySelector('#user-email');
-const userPassword = document.querySelector('#user-password');
-const seConnecter = document.querySelector('#connecter');
-const sInscrire = document.querySelector('#inscrire');
+const userLogIn = document.querySelector('#login'); // login page, login button
+const userSignUp = document.querySelector('#sign-up'); // loginpage, sign up button
 
+
+// class user
+class User {
+    userFirstName;
+    userLastName;
+    userEmail;
+    userTel;
+    userMobile;
+    userDateOfBirth;
+    userAddresses=[];
+    userCart=[];
+    userCartTotal=0;
+    userTreatment=[];
+    userFollowOrder=[];
+    userHistory=[];
+    loggedIn = true;
+
+    constructor(firstName, lastName, addresses, email, tel, mobile, dob){
+        this.userFirstName = firstName;
+        this.userLastName = lastName;
+        this.userAddresses = addresses;
+        this.userEmail = email;
+        this.userTel = tel;
+        this.userMobile = mobile;
+        this.userDateOfBirth = dob;
+    }
+
+} // end of class
+
+// create user
+let userFra = new User("Francesca", "Nadel", {"domicile":"Bordeaux"}, "fra@gmail.com", "0622000452", "0622000452", "30/05/1977")
+localStorage.setItem('user', JSON.stringify(userFra)); // create user in localStorage
+
+// retrieve user email input
+let userEmail="";
+function registerEmail(email){
+    userEmail=email;
+}
+
+// retrieve user password input
+let userPassword="";
+function registerPassword(password){
+    userPassword=password;
+}
+
+// check email and password, if True, go to account
+// if false....
+userLogIn.addEventListener("click", controlLogin =>{
+    if(userEmail === "francesca.nadel@gmail.com" && userPassword === "secret"){
+        window.location.href = "/pages/account.html";
+    } else {console.log("login error");
+        document.querySelector('#login-error').style.display = "block";}
+})
