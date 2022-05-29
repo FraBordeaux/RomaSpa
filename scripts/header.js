aafbfe2a5c92a61d7c335de5b16b7a5aec4e3c90
FraBordeaux <103116149+FraBordeaux@users.noreply.github.com>
2022-05-30 01:22:21 +0200

    blending

--- scripts/header.js
+++ scripts/header.js
@@ -1,23 +1,65 @@
-const header = document.querySelector("header");
 
-header.innerHTML =
-`
-    <!-- insert logo -->
-    <img src="/assets/images/logos/logowhite.png" alt="logo Roma SPA" id = "logo_romaheader">
+//menu html
 
-    <!-- what does this do ? -->
-    <div id="top_head_border"></div>
+document.querySelector('header').innerHTML = 
+        `
+        <div class="header_mobile" onclick="toggleMenu()">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </div>
 
+        <nav class="nav-mobile">
+            <div class="nav-mobile_close" onclick="toggleMenu()">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </div>
+            
+            <div class="menu_a">
+                <ul class="nav-mobile_menu">
+                    <li><a href="/pages/index.html">Accueil</a></li>
+                    <li><a href="/pages/treatments.html">Soins</a></li>
+                    <li><a href="/pages/products.html">Produits</a></li>
+                    <li><a href="/pages/gallery.html">Galerie</a></li>
+                    <li><a href="/pages/contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </nav> 
+            
+        <div class="mobile-overlay" onclick="toggleMenu()"></div>
+
+        <!--logo-->
+
+        <div id = "div_logo_romaheader">
+            <img src="../assets/images/logos/logowhite.png" alt="logo Roma SPA" id="logo_romaheader">
+            
+            <!--ligne sous le logo-->
+            <div id="div_border_line" ></div>
+        </div>
+
+        <!--icons-->
+        <div class = "icon_header">
+            <input type="image" src="../assets/images/icons/user_profil.png" id="user_profil">
+
+        <!--shoppingList-->
+            <input type="image" src="../assets/images/icons/buy_list.png" class="buy_list">
+            <div class="triangleUpShoppingList"></div>
+            <div class="divShoppingList"><p id="productsSelected"></p></div>    
+        </div>
+
+        
+        <!--video background-->
+        <img src="../assets/images/backgrounds/background-header.jpg" alt="salle de reception" id="background_image">
+
+        <div class="title-header">
+            <h1>Spa ROMA</h1>
+            <h2>Spa de Luxe</h2>
+        </div>
+      
 `;
 
-/* video en boucle. je n'ai pas reçu le fichier du video.
-    <video id="background_video" autoplay loop muted>
-        <source src="/assets/videos/background_video_header.mp4" type="video/mp4">  
-    </video>
-*/
-
 //Menu Bg
-
 function toggleMenu() {
     const navMobile = document.querySelector('.nav-mobile');
     const mobileOverlay = document.querySelector('.mobile-overlay');  
@@ -31,17 +73,14 @@ function toggleMenu() {
         navMobile.classList.add('active');
         mobileOverlay.classList.add('active');
         document.querySelector("svg").style.color = "transparent";
-
     } 
 }
 
-/*
 //Shoppingiconlist
 const triangle = document.querySelector('.triangleUpShoppingList')
 const divShoppingList = document.querySelector('.divShoppingList')
 const iconShoppingList = document.querySelector('.buy_list')
 const productsSelected = document.querySelector('#productsSelected')
-
 
 iconShoppingList.addEventListener("mouseover",function(){
     triangle.style.cssText = ` width: 0;
@@ -62,10 +101,6 @@ iconShoppingList.addEventListener("mouseover",function(){
     position: absolute;
     display: flex;
     align-items: center;`
-
-
-
-
 },true);
 
 iconShoppingList.addEventListener("mouseleave",function(){
@@ -73,10 +108,26 @@ iconShoppingList.addEventListener("mouseleave",function(){
     divShoppingList.style.cssText =`none`
 },true);
 
+// retrieve user signed-up
+let retrieveUserInfo = localStorage.getItem('user'); // format string
+let user = JSON.parse(retrieveUserInfo);// format objet
 
-*/
+
+// COMPTE COMPTE COMPTE
+// if user logged in, icon leads to account page
+// else it leads to login page
 
 
+// PANIER PANIER PANIER
+// if user logged in : personalized welcome message  
+divShoppingList.innerText = 
+`
+Bienvenue ${user.userFirstName}!
+`
+;
+    // if cart empty, display "panier vide"
+    // else display items in cart.
+    // use functions on account js page
 
-
+// else se connecter button that leads to login page
 
